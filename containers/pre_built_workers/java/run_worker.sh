93fb2c7d4fe1ce7aab9eee8c74f5c86dcc011768
Brandon Paiz <brandonpaiz@google.com>
2021-11-01 21:51:00 +0000

    Use nproc to determine processor count

---
+++ containers/pre_built_workers/java/run_worker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Copyright 2020 gRPC authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+echo "bp12345678"
+
+processor_count=$(nproc)
+echo "Processor count: ${processor_count}"
+
+while getopts s: flag; do
+  case "${flag}" in
+  s) input_server_port=${OPTARG} ;;
+  *) echo "usage: $0 -s [server_port]" >&1
+       exit 1 ;;
+  esac
+done
+
+echo "Driver port: ${DRIVER_PORT}"
+
+BENCHMARK_WORKER_OPTS="-XX:ActiveProcessorCount=${processor_count}" \
+  timeout --kill-after="${KILL_AFTER}" "${POD_TIMEOUT}" \
+  benchmarks/build/install/grpc-benchmarks/bin/benchmark_worker \
+  --driver_port="${DRIVER_PORT}"
